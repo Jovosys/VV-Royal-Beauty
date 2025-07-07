1bc6b9644a8d5ea45ca0151998388b1109e94ea5
Jovosys <jovosys@gmail.com>
2025-07-07 22:23:39 +0530

    Bridal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './Components Html/About'
+import Bridal from './Components Html/Bridal'
 import Footer from './Components Html/Footer'
 import Home from './Components Html/Home'
 import Navbars from './Components Html/Navbars'
@@ -11,11 +12,12 @@ function App() {
     <>
     <Navbars/>
     
-    {/* <Routes> */}
-      {/* <Route path="/" element={<Home/>} /> */}
+    <Routes>
+      <Route path="/" element={<Home/>} />
       <Home/>
-    {/* </Routes> */}
-    {/* <About/> */}
+    </Routes>
+    <About/>
+    <Bridal/>
       <Footer/>
     </>
   )
